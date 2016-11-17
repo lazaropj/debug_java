f7e1551a8e98f44268658aa0a9eb7b5fa72ef8aa
Lazaro Jr <linkout.lazaro@gmail.com>
2016-11-17 12:46:29 -0200

    adicionando classe nova

---
+++ src/main/java/br/com/alura/debug/debugStore/RodarAplicacao.java
@@ -0,0 +1,38 @@
+package br.com.alura.debug.debugStore;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.alura.debug.debugStore.modelo.CarrinhoCompra;
+import br.com.alura.debug.debugStore.modelo.Categoria;
+import br.com.alura.debug.debugStore.modelo.FormaPagamento;
+import br.com.alura.debug.debugStore.modelo.Produto;
+
+public class RodarAplicacao {
+	
+	public static void main(String[] args) {
+		Categoria categoria = new Categoria("Informatica");
+		List<Produto> produtos = criarProdutos(categoria);
+		CarrinhoCompra carrinho = new CarrinhoCompra(FormaPagamento.BOLETO);
+		BigDecimal valorFinalDaCompra = carrinho.calcularPrecoFinal(produtos);
+		System.out.println("Valor da compra: " + valorFinalDaCompra);
+	}
+
+	
+
+	private static List<Produto> criarProdutos(Categoria categoria) {
+		List<Produto> produtos = new ArrayList<Produto>();
+		Produto mouse = new Produto("Mouse sem fio Microsoft",new BigDecimal(("120.00")), categoria);
+		produtos.add(mouse);
+		Produto teclado = new Produto("Teclado de Gamer Alien", new BigDecimal("350.00"), categoria);
+		produtos.add(teclado);
+		Produto monitor = new Produto("Monitor Lg 17 Widescreen", new BigDecimal("250.00"), categoria);
+		produtos.add(monitor);
+		Produto processador = new Produto("Processador Intel Core I7", new BigDecimal("1500.00"), categoria);
+		produtos.add(processador);
+		Produto cadeira = new Produto("Cadeira Racing Gamer", new BigDecimal("759.00"), categoria);
+		produtos.add(cadeira);
+		return produtos;
+	}
+}
